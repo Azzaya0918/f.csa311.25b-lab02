021d4cf09f496101398041b77086ae20915791bc
Azzaya0918 <azzayabat2020@gmail.com>
2026-03-04 12:03:29 +0800

    azaa

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,21 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape shape = new Rectangle(2, 3);
+        Shape shape2 = new Circle(2);
+        Shape shape3 = new Square(3);
 
-        Renderer renderer = new Renderer(rectangle);
-
+        Renderer renderer = new Renderer(shape);
         renderer.draw();
+        Renderer renderer2 = new Renderer(shape2);
+        renderer2.draw();
+        Renderer renderer3 = new Renderer(shape3);
+        renderer3.draw();
     }
 }
